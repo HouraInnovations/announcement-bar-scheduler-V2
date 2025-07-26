8579f98074db0563851142e41feaedfdb47e659c
Aaron <info@personalisable.co.uk>
2025-07-26 23:44:33 +0100

    Fix remix config to exclude Shopify modules from bundle

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,10 +1,7 @@
-// Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
-// Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
-// stop passing in HOST, so we can remove this workaround after the next major release.
+// Fix for HOST env var per https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 if (
   process.env.HOST &&
-  (!process.env.SHOPIFY_APP_URL ||
-    process.env.SHOPIFY_APP_URL === process.env.HOST)
+  (!process.env.SHOPIFY_APP_URL || process.env.SHOPIFY_APP_URL === process.env.HOST)
 ) {
   process.env.SHOPIFY_APP_URL = process.env.HOST;
   delete process.env.HOST;
@@ -17,10 +14,18 @@ module.exports = {
   serverModuleFormat: "cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
 
-  // Tell Remix NOT to bundle these server dependencies (Shopify modules)
+  // Here is the important part:
+  // Empty array means don't bundle any dependencies on server side.
+  // This will mark all node_modules as external.
   serverDependenciesToBundle: [],
 
   future: {
-    v2_routeConvention: true,
+    // Remove obsolete flags - v2_routeConvention is obsolete, so remove it.
+    // Add any new flags you want here, e.g.:
+    v3_fetcherPersist: true,
+    v3_lazyRouteDiscovery: true,
+    v3_relativeSplatPath: true,
+    v3_singleFetch: true,
+    v3_throwAbortReason: true,
   },
 };
