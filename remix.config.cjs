4bc5fae82c6d729947dfa761b01b2d9f2493b548
Aaron <info@personalisable.co.uk>
2025-07-26 23:52:46 +0100

    Fix remix config to exclude Shopify modules from bundle and update future flags

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,4 +1,4 @@
-// Fix for HOST env var per https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
+// Workaround for HOST env var issue per https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 if (
   process.env.HOST &&
   (!process.env.SHOPIFY_APP_URL || process.env.SHOPIFY_APP_URL === process.env.HOST)
@@ -14,14 +14,11 @@ module.exports = {
   serverModuleFormat: "cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
 
-  // Here is the important part:
-  // Empty array means don't bundle any dependencies on server side.
-  // This will mark all node_modules as external.
+  // Don't bundle any node_modules on the server side (keep Shopify imports external)
   serverDependenciesToBundle: [],
 
   future: {
-    // Remove obsolete flags - v2_routeConvention is obsolete, so remove it.
-    // Add any new flags you want here, e.g.:
+    // Use new Remix future flags as recommended by v2.13+
     v3_fetcherPersist: true,
     v3_lazyRouteDiscovery: true,
     v3_relativeSplatPath: true,
